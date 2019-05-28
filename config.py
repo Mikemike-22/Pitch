be2c2815aa653ba3c067ac572be9f92b31ef5086
Michael Korir <mikor22@gmail.com>
2019-05-28 12:07:53 +0300

    create app instance

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+class Config:
+    
+    pass
+
+
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+
+    pass
+    
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}

d99625e718e07c7c935f1421b8fb646faabe3289
Michael Korir <mikor22@gmail.com>
2019-05-29 10:58:08 +0300

    update configurations

--- config.py
+++ config.py
@@ -2,22 +2,46 @@ import os
 
 class Config:
 
-    pass
-
-
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://antavio:pass123@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    # Gmail Configs
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
+    '''
+    Production configuration child class
+    Args:
+        Config:The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
-    pass
-    
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://antavio:pass123@localhost/pitch_test'
 
 
 class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://antavio:pass123@localhost/pitch'
     DEBUG = True
 
+    
+
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
 }
